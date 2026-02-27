75aea2c138d7e7b86dcdbfc1ca1b782b1e99f05e
Jaco Venter <149387555+Jacobventer@users.noreply.github.com>
2026-02-27 15:57:31 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,19 +5,14 @@ from fastapi import FastAPI, HTTPException
 from app.schemas import PredictionRequest, PredictionResponse
 from src.inference import predict_from_xml
 
-app = FastAPI(
-    title="Marvelous Insurance Destruction Predictor",
-    version="1.0"
-)
+app = FastAPI(title="Marvelous Insurance Destruction Predictor", version="1.0")
 
 @app.post("/predict", response_model=PredictionResponse)
 def predict(request: PredictionRequest):
     try:
         prediction = predict_from_xml(request.credit_info_xml)
 
-        return PredictionResponse(
-            predicted_destruction_events=prediction
-        )
+        return PredictionResponse(predicted_destruction_events=prediction        )
 
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
